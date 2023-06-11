b09bf7984f0b927bcd053502f2eadd8ab1bab37c
narisada014 <nari.reishi@gmail.com>
2023-06-11 22:09:21 +0900

    detect request

--- nodejs/src/connect.ts
+++ nodejs/src/connect.ts
@@ -6,6 +6,7 @@ export default (router: ConnectRouter) =>
   router.service(ElizaService, {
     // implements rpc Say
     async say(req: SayRequest) {
+      console.log(req)
       console.log("Requested: ", req.sentence);
       return {
         sentence: `You said: ${req.sentence}`,
