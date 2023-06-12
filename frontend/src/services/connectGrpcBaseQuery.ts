a02095bed85bdc0f29f48e4cb3b3b495a4d8b278
narisada014 <nari.reishi@gmail.com>
2023-06-12 22:26:08 +0900

    refactoring: separate base file

---
+++ frontend/src/services/connectGrpcBaseQuery.ts
@@ -0,0 +1,18 @@
+import type { ServiceType } from "@bufbuild/protobuf"
+import { PromiseClient } from "@bufbuild/connect"
+import { BaseQueryFn } from '@reduxjs/toolkit/query/react'
+import { client as connectClient } from "./client";
+
+export const connectGrpcBaseQuery = <T extends ServiceType>(service: T): BaseQueryFn<
+  { req: (client: PromiseClient<T>) => Promise<unknown> },
+  unknown,
+  unknown
+> => async ({ req }, api, extraOptions) => {
+    const client = connectClient(service);
+    try {
+      const response = await req(client);
+      return { data: response };
+    } catch (error) {
+      return { error: { message: "error raised" } };
+    }
+  }
--- frontend/src/services/eliza.ts
+++ frontend/src/services/eliza.ts
@@ -1,24 +1,8 @@
-import type { ServiceType } from "@bufbuild/protobuf"
 import { PromiseClient } from "@bufbuild/connect"
-import { BaseQueryFn } from '@reduxjs/toolkit/query/react'
-import { client as connectClient } from "./client";
 import { createApi } from "@reduxjs/toolkit/query/react";
-import {ElizaService} from "../../gen/eliza_connect";
+import { ElizaService } from "../../gen/eliza_connect";
 import { SayRequest, SayResponse } from "../../gen/eliza_pb";
-
-export const connectGrpcBaseQuery = <T extends ServiceType>(service: T): BaseQueryFn<
-  { service: T; req: (client: PromiseClient<T>) => Promise<unknown> },
-  unknown,
-  unknown
-> => async ({ service, req }, api, extraOptions) => {
-    const client = connectClient(service);
-    try {
-      const response = await req(client);
-      return { data: response };
-    } catch (error) {
-      return { error: { message: "error raised" } };
-    }
-  }
+import { connectGrpcBaseQuery } from "./connectGrpcBaseQuery";
 
 export const elizaApi = createApi({
   reducerPath: "elizaApi",
@@ -26,13 +10,7 @@ export const elizaApi = createApi({
   endpoints: (builder) => ({
     helloEliza: builder.query<string, string>({
       query: (sentence) => ({
-        service: ElizaService,
-        req: async (client: PromiseClient<typeof ElizaService>) => {
-          const req = new SayRequest();
-          req.sentence = sentence
-          const res = await client.say(req)
-          return res
-        },
+        req: async (client: PromiseClient<typeof ElizaService>) => client.say(new SayRequest({sentence: sentence})),
       }),
       transformResponse: (response: SayResponse) => {
         // シリアライズ可能なデータに変換する必要がある
